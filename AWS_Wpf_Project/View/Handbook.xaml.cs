6f9506866c037f61dcffaea9ff600a627a41ac6a
Selyakov V.A <vyacheslav.selyakov@mail.ru>
2019-09-07 14:55:43 +0300

    Создана структура проекта. Окно Справочники только создано и добавлены вкладки. Сделаны заготовки для создания стилей.

---
+++ AWS_Wpf_Project/View/Handbook.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AWS_Wpf_Project.View
+{
+    /// <summary>
+    /// Логика взаимодействия для Handbook.xaml
+    /// </summary>
+    public partial class Handbook : Window
+    {
+        public Handbook()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AWS_Wpf_Project/ViewModel/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AWS_Wpf_Project.View;
+
+namespace AWS_Wpf_Project.ViewModel
+{
+    public class MainViewModel
+    {
+        #region fields
+
+        private RelayCommand _openHandbooksCommand;
+
+        #endregion
+
+        #region Commands
+
+        public RelayCommand OpenHandbookCommand
+        {
+            get
+            {
+                return _openHandbooksCommand ??
+                  (_openHandbooksCommand = new RelayCommand(obj =>
+                  {
+                      Handbook handbook = new Handbook();
+                      handbook.Show();
+                  }));
+            }
+        }
+
+        #endregion
+    }
+}
